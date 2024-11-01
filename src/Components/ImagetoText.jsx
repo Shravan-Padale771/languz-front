b2b9a20b5fdfd247f48a10df8f230ff5e34b1b3c
Shravan Padale <74761476+Shravan-Padale771@users.noreply.github.com>
2024-11-01 17:14:31 +0530

    Update ImagetoText.jsx

--- src/Components/ImagetoText.jsx
+++ src/Components/ImagetoText.jsx
@@ -8,9 +8,11 @@ function ImagetoText() {
   const [translatedText, setTranslatedText] = useState('');
   const [Text, setText] = useState('');
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   // Function to handle file upload and OCR process
   async function processImg(img,lang) {
+    setLoading(true)
     const formData = new FormData();
     formData.append('image', img);
     formData.append('lang', lang);
@@ -28,6 +30,7 @@ function ImagetoText() {
       const data = await response.json();
       setText(data.text); // Display recognized text
       setTranslatedText(data.translatedText);
+      setLoading(false)
     } catch (error) {
       setError('Error processing image or language not detected.');
       console.error(error);
@@ -40,7 +43,31 @@ function ImagetoText() {
      
 
       <Input uploadImage={processImg} />
+
+      {loading && (
+        <div className="flex items-center justify-center mt-5">
+          <svg className="animate-spin h-8 w-8 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+            ></path>
+          </svg>
+          <p className="ml-2 text-blue-500 font-semibold">Translating... Please wait</p>
+        </div>
+      )}
+
+
       {error && <p className='text-center text-red-800'>{error}</p>}
+
      {Text ? <Textarea heading="Image Your Text" text={Text} /> : null }
      {translatedText ? <Textarea heading="Translated" text={translatedText} /> : null }
 
